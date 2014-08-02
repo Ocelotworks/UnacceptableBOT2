6f9f9ca1e24ed769e3082a7429c02de93ebda349
UnacceptableUse <unacceptableuse@gmail.com>
2014-08-02 14:04:38 +0100

    read

--- src/com/unacceptableuse/unacceptablebot/handler/ConfigHandler.java
+++ src/com/unacceptableuse/unacceptablebot/handler/ConfigHandler.java
@@ -3,7 +3,9 @@ package com.unacceptableuse.unacceptablebot.handler;
 public class ConfigHandler
 {
 
-	
+	/*
+	 * THIS IS WHERE THE BOT WILL CONTACT THE DATABASE, ANY CONFIGURATION WILL BE DRAWN FROM HERE
+	 */
 	
 	
 }
