40ec2aedd73b9350fbce6572d00616a92964e96b
Neil Trotter <ntrotter699@gmail.com>
2014-08-03 14:26:41 +0100

    Started work on FillMeIn

---
+++ src/com/unacceptableuse/unacceptablebot/command/CommandFillMeIn.java
@@ -0,0 +1,19 @@
+package com.unacceptableuse.unacceptablebot.command;
+
+import org.pircbotx.PircBotX;
+
+public class CommandFillMeIn extends Command {
+
+	@Override
+	public void performCommand(String target, String[] args, String message,
+			PircBotX bot) {
+		
+		
+	}
+
+	@Override
+	public String[] getAliases() {
+		return new String[]{"fillmein","fmi"};
+	}
+
+}
