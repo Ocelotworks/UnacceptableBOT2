c42aeec093d1bdddd2c4fdf9f88b7a4d0bfd95af
UnacceptableUse <unacceptableuse@gmail.com>
2014-08-08 21:10:51 +0100

    Changes to initializer

--- src/com/unacceptableuse/unacceptablebot/Initializer.java
+++ src/com/unacceptableuse/unacceptablebot/Initializer.java
@@ -1,9 +1,12 @@
 package com.unacceptableuse.unacceptablebot;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
-
-import com.unacceptableuse.unacceptablebot.handler.CommandHandler;
+import org.slf4j.Logger;
 
 public class Initializer
 {
@@ -11,22 +14,32 @@ public class Initializer
 	
 	public Initializer(String username)
 	{	
-		Configuration config = new Configuration.Builder()
-						.setName(username)
-						.setLogin(username)
-						.setAutoNickChange(true)
-						.addListener(new UnacceptableBot())
-						.setServerHostname("peter.unacceptableuse.com")
-						.setServerPort(6868)
-						.setServerPassword("Stevie/Freenode:botbot")
-						.addAutoJoinChannel("##UBTesting")
-						.buildConfiguration();
-		
+		System.out.println("Loading config...");
 		try
 		{
+			InputStream is = new FileInputStream("static.properties");
+			Properties props = new Properties();
+			props.load(is);
+			
+			Configuration config = new Configuration.Builder()
+			.setName(username)
+			.setLogin(username)
+			.setAutoNickChange(true)
+			.addListener(new UnacceptableBot())
+			.setServerHostname(props.getProperty("server"))
+			.setServerPort(Integer.parseInt(props.getProperty("port")))
+			.setServerPassword(props.getProperty("password"))
+			.addAutoJoinChannel(props.getProperty("default_channel"))
+			.buildConfiguration();
+			
+			props.clear();
+			is.close();
+			 
 			PircBotX bot = new PircBotX(config);
 			bot.startBot();
-		}
+			
+			
+		} 
 		catch(Exception e)
 		{
 			e.printStackTrace();
@@ -37,6 +50,6 @@ public class Initializer
 	
 	public static void main(String[] args) throws Exception
 	{
-		new Initializer("UnacceptableBOT");
+		new Initializer("Stevie");
 	}
 }
