3f8d35f8e97cefac3e5144f883d239bc9aebb66c
UnacceptableUse <unacceptableuse@gmail.com>
2014-08-09 23:15:21 +0100

    ooo

---
+++ src/com/unacceptableuse/unacceptablebot/command/CommandRPS.java
@@ -0,0 +1,73 @@
+package com.unacceptableuse.unacceptablebot.command;
+
+import java.util.Date;
+
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+
+import com.unacceptableuse.unacceptablebot.UnacceptableBot;
+
+public class CommandRPS extends Command
+{
+
+	@Override
+	public void performCommand(User sender, Channel channel, String message,
+			String[] args, PircBotX bot)
+	{
+		
+		long lastFaucet = UnacceptableBot.getConfigHandler().getLong("faucetAt:"+sender.getUserId().toString());
+		int faucetTimeout = UnacceptableBot.getConfigHandler().getInteger("faucetTimeout");
+		if(lastFaucet != 0L)
+		{
+			if(new Date().getTime()-lastFaucet < faucetTimeout)
+			{
+				sendMessage(bot, "You must wait another "+(faucetTimeout-(new Date().getTime()-lastFaucet))+" seconds until you can receive doge!", channel.getName());
+				return;
+			}
+		}	
+			
+		//1 = ROCK 2 = PAPER 3 = SCISSORS
+			
+		int playerChoice = args[1].equalsIgnoreCase("rock") ? 1 : args[1].equalsIgnoreCase("paper") ? 2 : args[1].equalsIgnoreCase("scissors") ? 3 : 0;
+		if(playerChoice == 0)
+		{
+			sendMessage(bot, "Unknown choice. Try rock, paper or scissors.", channel);
+			return;
+		}else
+		{
+			int steveChoice = (UnacceptableBot.rand.nextInt(300)/100)+1;
+			boolean playerWon = playerChoice == steveChoice ? false : playerChoice == 1 && steveChoice == 2 ? false : playerChoice == 3 && steveChoice == 1 ? false : playerChoice == 2 && steveChoice == 3 ? false :  true; 
+			sendMessage(bot, (playerWon ? "You WIN! " : steveChoice == playerChoice ? "You TIED! " : "You LOSE! ")+UnacceptableBot.getConfigHandler().getString("botName")+" chose "+getAction(steveChoice)+" and you chose "+getAction(playerChoice), channel);
+			if(playerWon && !UnacceptableBot.getConfigHandler().getBoolean("rpsBounty"))
+				sendMessage(bot, ".tip "+sender+" "+UnacceptableBot.rand.nextInt(UnacceptableBot.getConfigHandler().getInteger("faucetLimit"))+1, channel);
+			
+			UnacceptableBot.getConfigHandler().setLong("faucetAt:"+sender.getUserId().toString(), new Date().getTime());
+		}
+				
+		
+	}
+	
+	private String getAction(int num)
+	{
+		switch(num)
+		{
+		case 1:
+			return "rock";
+		case 2:
+			return "paper";
+		case 3:
+			return "scissors";
+		default:
+			return "error ("+num+")";
+		}
+	}
+
+	@Override
+	public String[] getAliases()
+	{
+		
+		return null;
+	}
+
+}
