e215f6b429f55f636976c0fc0bbbc33d67a9da69
UnacceptableUse <unacceptableuse@gmail.com>
2014-08-11 22:54:15 +0100

    Fixed some security holes

--- src/com/unacceptableuse/unacceptablebot/command/CommandQuote.java
+++ src/com/unacceptableuse/unacceptablebot/command/CommandQuote.java
@@ -15,9 +15,10 @@ public class CommandQuote extends Command {
 	@Override
 	public void performCommand(User sender, Channel channel, String message,
 			String[] args, PircBotX bot) {
-		String user = args[1];
 		try{
-			PreparedStatement ps = UnacceptableBot.getConfigHandler().sql.getPreparedStatement("SELECT Message FROM `teknogeek_unacceptablebot`.`" + args.length == 3 ? (args[2].startsWith("#") ? args[2] : "#" + args[2]) : channel.getName() + "` WHERE Username = '"+user+"' ORDER BY RAND() LIMIT 1");
+			PreparedStatement ps = UnacceptableBot.getConfigHandler().sql.getPreparedStatement("SELECT Message FROM `teknogeek_unacceptablebot`.`?` WHERE Username = '?' ORDER BY RAND() LIMIT 1");
+			ps.setString(1, args.length == 3 ? args[2].startsWith("#") ? args[2] : "#" + args[2] : channel.getName());
+			ps.setString(2, args[1]);
 			ResultSet rs = ps.executeQuery();
 			bot.sendIRC().message(channel.getName(), rs.getString(1));
 		} catch(SQLException e){
@@ -29,6 +30,12 @@ public class CommandQuote extends Command {
 	@Override
 	public String[] getAliases() {
 		return new String[]{"quote"};
+	}
+	
+	@Override
+	public int requiredArguments()
+	{
+		return 1;
 	}
 
 }
