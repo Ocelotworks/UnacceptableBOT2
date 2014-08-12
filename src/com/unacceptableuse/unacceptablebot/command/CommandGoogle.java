5a148cba98f0c63b1856a087f3e3b4961d4f0c75
teknogeek <jmargolis97@gmail.com>
2014-08-11 20:50:18 -0400

    fix LMGTFY

--- src/com/unacceptableuse/unacceptablebot/command/CommandGoogle.java
+++ src/com/unacceptableuse/unacceptablebot/command/CommandGoogle.java
@@ -4,21 +4,24 @@ import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 
-public class CommandGoogle extends Command {
+public class CommandGoogle extends Command
+{
 
 	@Override
-	public void performCommand(User sender, Channel channel, String message,
-			String[] args, PircBotX bot) {
-		String add = "www.lmgtfy.com/?q=";
-		for(int i = 1; i<args.length - 1; i++){
-			add = add + args[i];
+	public void performCommand(User sender, Channel channel, String message, String[] args, PircBotX bot)
+	{
+		String add = "http://www.lmgtfy.com/?q=";
+		for(int i = 1; i < args.length; i++)
+		{
+			add += args[i] + "+";
 		}
+		add = add.substring(0, add.length() - 1);
 		bot.sendIRC().message(channel.getName(), add);
-		
 	}
 
 	@Override
-	public String[] getAliases() {
+	public String[] getAliases()
+	{
 		return new String[]{"google", "lmgtfy"};
 	}
 	
