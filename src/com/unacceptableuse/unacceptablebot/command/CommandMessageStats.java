06b8dac7ff5a8d930a5bf0fc2ccbbf9dd2bf47b3
eduardog3000 <eduardog3000@gmail.com>
2014-09-17 22:14:16 -0400

    Fixed CommandMessageStats
    Just had to replace "channel" with "channel.getName()" in a couple
    spots.

--- src/com/unacceptableuse/unacceptablebot/command/CommandMessageStats.java
+++ src/com/unacceptableuse/unacceptablebot/command/CommandMessageStats.java
@@ -12,60 +12,54 @@ import org.pircbotx.User;
 public class CommandMessageStats extends Command {
 
 	@Override
-	public void performCommand(User sender, Channel channel, String message,
-			String[] args) {
+	public void performCommand(User sender, Channel channel, String message, String[] args) {
 		final String target = args.length > 1 ? args[1] : sender.getNick();
 
 		try {
-
-			int totalCount = 0;// , totalLines = 0;
+			int totalCount = 0;
 			HashMap<String, Integer[]> count = new HashMap<String, Integer[]>();
 			File[] files = new File("./").listFiles();
 			ArrayList<File> logs = new ArrayList<File>();
+			@SuppressWarnings("unused")
 			long oldestFileTime = System.currentTimeMillis();
-			if (oldestFileTime == 0) {} //Here to stop the unused var pestering the error console
 			for (File f : files) {
-				if (f.isDirectory() || !f.canRead())
+				if (f.isDirectory() || !f.canRead()) {
 					continue;
-				if (f.getName().charAt(0) == '#' && !f.getName().contains("copy")) {
+				} else if (f.getName().charAt(0) == '#' && !f.getName().contains("copy")) {
 					logs.add(f);
 				}
-
 			}
 
-			sendMessage("Processing " + logs.size()
-					+ " logs... This might take a while", channel);
+			sendMessage("Processing " + logs.size() + " logs... This might take a while", channel);
 			for (File log : logs) {
 				int icount = 0;
 				int totalLines = 0;
-				BufferedReader br = new BufferedReader(new FileReader(channel
-						+ ".ub2log"));
+				BufferedReader br = new BufferedReader(new FileReader(channel.getName() + ".ub2log"));
 				String line = br.readLine();
 				while ((line = br.readLine()) != null) {
-					if (line.toLowerCase().contains(target.toLowerCase()))
+					if (line.toLowerCase().contains(target.toLowerCase())) {
 						icount++;
+					}
 					totalLines++;
 				}
-				count.put(log.getName(), new Integer[] { icount, totalLines });
+				count.put(log.getName(), new Integer[] {icount, totalLines});
 				br.close();
-
 			}
 
 			for (String s : count.keySet()) {
 				totalCount += count.get(s)[0];
-				// totalLines+= count.get(s)[1];
 			}
 
 			sendMessage(
 					"&BOLD"
 							+ target
 							+ "&RESET has sent &BOLD"
-							+ count.get(channel + ".log")[0]
+							+ count.get(channel.getName() + ".log")[0]
 							+ "&RESET messages in &BOLD"
-							+ channel
+							+ channel.getName()
 							+ "&RESET. &BOLD"
-							+ (count.get(channel + ".log")[0] * 100 / count
-									.get(channel + ".log")[1])
+							+ (count.get(channel + ".log")[0] * 100 / count.get(channel
+									.getName() + ".log")[1])
 							+ "&RESET% of all messages.", channel);
 			sendMessage(
 					"&BOLD"
@@ -76,7 +70,6 @@ public class CommandMessageStats extends Command {
 							+ count.size()
 							+ "&RESET channels. &BOLDFuck knows I CBA fixing this&RESET% of all messages.",
 					channel);
-
 		} catch (Exception e) {
 			sendMessage(
 					"&REDSHIT!&RESET: " + e.getMessage() + " "
@@ -86,8 +79,7 @@ public class CommandMessageStats extends Command {
 
 	@Override
 	public String[] getAliases() {
-
-		return new String[] { "messagestats" };
+		return new String[] {"messagestats"};
 	}
 
 	@Override
