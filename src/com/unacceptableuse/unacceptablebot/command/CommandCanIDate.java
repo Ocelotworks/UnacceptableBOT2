8de0287577f9f1a3c24dd141732b2abd759b48e1
UnacceptableUse <unacceptableuse@gmail.com>
2014-10-11 22:23:38 +0100

    Good enough

--- src/com/unacceptableuse/unacceptablebot/command/CommandCanIDate.java
+++ src/com/unacceptableuse/unacceptablebot/command/CommandCanIDate.java
@@ -11,7 +11,26 @@ public class CommandCanIDate extends Command
 			String[] args)
 	{
 		int dateAge = Integer.parseInt(args[2]), ourAge = Integer.parseInt(args[1]);
-		sendMessage((dateAge/2)+7 < ourAge ? "She's too young bro. That's a bit weird" : (ourAge > 15 ? "Bang" : "Finger")+" away my friend", channel);
+		//sendMessage((ourAge/2)+7 >= dateAge ? (dateAge/2)+7 >= ourAge ? (dateAge >= 16 && ourAge < 16) ? "You're too young... This has statutory rape written all over it!" : "You're a bit young dude" : (dateAge >= 16 && ourAge >= 16 ? "Bang" : "Finger")+" away my friend" : "She's a bit young bro... That's weird...", channel);
+		if((ourAge/2)+7 >= dateAge)
+		{
+			if((dateAge/2)+7 >= ourAge)
+			{
+				if(dateAge >= 16 && ourAge < 16)
+				{
+					sendMessage("You're too young, and this has statutory rape written all over it!", channel);
+				}else
+				{
+					sendMessage("You're too young... That's a bit weird", channel);
+				}
+			}else
+			{
+				sendMessage((dateAge >= 16 && ourAge >= 16 ? "Bang" : "Finger")+" away my friend", channel);
+			}
+		}else
+		{
+			sendMessage("She's too young bro. That's "+(dateAge < 12 ? "very" : "a little")+" weird", channel);
+		}
 		
 	}
 
