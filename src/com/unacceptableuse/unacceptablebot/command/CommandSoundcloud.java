89f4e6a07c73d2bd69b9d4cd899be4584a25db58
Peter Maguire <peter@unacceptableuse.com>
2015-01-09 18:38:45 +0000

    Fixed !soundcloud

--- src/com/unacceptableuse/unacceptablebot/command/CommandSoundcloud.java
+++ src/com/unacceptableuse/unacceptablebot/command/CommandSoundcloud.java
@@ -1,6 +1,7 @@
 package com.unacceptableuse.unacceptablebot.command;
 
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
 
 import org.pircbotx.Channel;
 import org.pircbotx.User;
@@ -27,13 +28,18 @@ public class CommandSoundcloud extends Command
 	@Override
 	public void performCommand(final User sender, final Channel channel, final String message, final String[] args)
 	{
-		final int index = message.contains("--index=") ? Integer.parseInt(message.split("index=")[1].split(" ")[0]) : 0;
-		final String query = message.replace(args[0] + " ", "");
-
-		final JsonArray je = UnacceptableBot.getParser().parse(new InputStreamReader(UnacceptableBot.getUrlContents("https://api.soundcloud.com/tracks.json?q=" + query + "&client_id=" + UnacceptableBot.getConfigHandler().getPassword("soundcloud_client")))).getAsJsonArray();
-		final JsonObject jo = je.get(index).getAsJsonObject();
-
-		sendMessage(jo.get("title").getAsString() + ": " + jo.get("permalink_url").getAsString(), channel);
+		try{
+			final int index = message.contains("--index=") ? Integer.parseInt(message.split("index=")[1].split(" ")[0]) : 0;
+			final String query = message.replace(args[0] + " ", "");
+	
+			final JsonArray je = UnacceptableBot.getParser().parse(new InputStreamReader(UnacceptableBot.getUrlContents("https://api.soundcloud.com/tracks.json?q=" + URLEncoder.encode(query, "UTF-8") + "&client_id=" + UnacceptableBot.getConfigHandler().getPassword("soundcloud_client")))).getAsJsonArray();
+			final JsonObject jo = je.get(index).getAsJsonObject();
+	
+			sendMessage(jo.get("title").getAsString() + ": " + jo.get("permalink_url").getAsString(), channel);
+		}catch(Exception e)
+		{
+			sendMessage("An error occurred proccessing your search: "+e.toString(), channel);
+		}
 
 	}
 
